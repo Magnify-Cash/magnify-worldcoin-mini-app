104f0acbfce08c4ab6c77e3a6006267c9259f19c
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-12-24 14:08:22 -0600

    update repay loan hook

--- src/hooks/useRepayLoan.tsx
+++ src/hooks/useRepayLoan.tsx
@@ -65,7 +65,7 @@ const useRepayLoan = () => {
                 type: "function",
               },
             ],
-            functionName: "requestLoan",
+            functionName: "repayLoan",
             args: [],
           },
         ],
